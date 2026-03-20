773621a10e7a884bd7fe791096b65487fe91c62f
Thong LT <letrthong@gmail.com>
2026-03-20 15:22:45 +0700

    Update start_systemd.sh

--- systemd/start_systemd.sh
+++ systemd/start_systemd.sh
@@ -1,5 +1,5 @@
 
-cp -fv ../sync-git.service /etc/systemd/system/
+cp -fv ./sync-git.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start sync-git
 systemctl enable sync-git # Tự chạy khi khởi động máy
